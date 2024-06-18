e5cae5f30283c881927cf4cda8e21ff477479c37
Fernando Alemán <fernandoaleman@mac.com>
2024-06-18 14:03:27 -0500

    Add toggle between two files keymap

--- lua/custom/core/keymaps.lua
+++ lua/custom/core/keymaps.lua
@@ -6,6 +6,8 @@ keymap.set("i", "jk", "<ESC>", { desc = "Exit insert mode with jk" })
 
 keymap.set("n", "<leader>nh", ":nohl<CR>", { desc = "Clear search highlights" })
 
+keymap.set("n", "<leader><leader>", "<C-^>", { desc = "Toggle between the last two files" })
+
 -- increment/decrement numbers
 keymap.set("n", "<leader>+", "<C-a>", { desc = "Increment number" }) -- increment
 keymap.set("n", "<leader>-", "<C-x>", { desc = "Decrement number" }) -- decrement
