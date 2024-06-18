3a15dd6f73908ccb8857f253203a99b8188c2ba2
Fernando Alemán <fernandoaleman@mac.com>
2024-06-18 14:04:30 -0500

    Add right and left indentation keymaps

--- lua/custom/core/keymaps.lua
+++ lua/custom/core/keymaps.lua
@@ -23,3 +23,7 @@ keymap.set("n", "<leader>tx", "<cmd>tabclose<CR>", { desc = "Close current tab" 
 keymap.set("n", "<leader>tn", "<cmd>tabn<CR>", { desc = "Go to next tab" }) --  go to next tab
 keymap.set("n", "<leader>tp", "<cmd>tabp<CR>", { desc = "Go to previous tab" }) --  go to previous tab
 keymap.set("n", "<leader>tf", "<cmd>tabnew %<CR>", { desc = "Open current buffer in new tab" }) --  move current buffer to new tab
+
+-- visual
+keymap.set("v", "<", "<gv", { desc = "Stay in right indentation mode" }) -- right indentation
+keymap.set("v", ">", ">gv", { desc = "Stay in left indentation mode" }) -- left indentation
