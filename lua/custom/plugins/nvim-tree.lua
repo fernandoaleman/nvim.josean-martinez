46d53d67b0f1dcc5684ee55ab009cba37e3dc0e4
Fernando Alemán <fernandoaleman@mac.com>
2024-06-24 17:20:22 -0500

    Update nvim-tree settings

--- lua/custom/plugins/nvim-tree.lua
+++ lua/custom/plugins/nvim-tree.lua
@@ -8,10 +8,21 @@ return {
     vim.g.loaded_netrw = 1
     vim.g.loaded_netrwPlugin = 1
 
+    -- change color for arrows in tree to light blue
+    vim.cmd([[ highlight NvimTreeFolderArrowClosed guifg=#3FC5FF ]])
+    vim.cmd([[ highlight NvimTreeFolderArrowOpen guifg=#3FC5FF ]])
+    vim.cmd([[ highlight NvimTreeGitDirty guifg=#ffd93f ]])   -- unstaged   (yellow)
+    vim.cmd([[ highlight NvimTreeGitStaged guifg=#3fff79 ]])  -- staged     (green)
+    vim.cmd([[ highlight NvimTreeGitMerge guifg=#ffd93f ]])   -- unmerged   (yellow)
+    vim.cmd([[ highlight NvimTreeGitRenamed guifg=#ffd93f ]]) -- renamed    (yellow)
+    vim.cmd([[ highlight NvimTreeGitNew guifg=#ff3f65 ]])     -- untracked  (red)
+    vim.cmd([[ highlight NvimTreeGitDeleted guifg=#ff3f65 ]]) -- deleted    (red)
+    vim.cmd([[ highlight NvimTreeGitIgnored guifg=#ffd93f ]]) -- ignored    (yellow)
+
     nvimtree.setup({
       view = {
         width = 35,
-        relativenumber = true,
+        relativenumber = false,
       },
       -- change folder arrow icons
       renderer = {
@@ -23,6 +34,15 @@ return {
             folder = {
               arrow_closed = "", -- arrow when folder is closed
               arrow_open = "", -- arrow when folder is open
+            },
+            git = {
+              unstaged = "",
+              staged = "✓",
+              unmerged = "",
+              renamed = "➜",
+              untracked = "",
+              deleted = "✕",
+              ignored = "◌",
             },
           },
         },
