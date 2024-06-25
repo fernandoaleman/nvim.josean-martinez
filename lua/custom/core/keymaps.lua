95c21fe2ef84b020f55760209fde2a2161c83da5
Fernando Alemán <fernandoaleman@mac.com>
2024-06-24 19:32:43 -0500

    Add terraform linting and keymaps

--- lua/custom/core/keymaps.lua
+++ lua/custom/core/keymaps.lua
@@ -21,9 +21,16 @@ keymap.set("n", "<leader>sx", "<cmd>close<CR>", { desc = "Close current split" }
 keymap.set("n", "<leader>to", "<cmd>tabnew<CR>", { desc = "Open new tab" }) -- open new tab
 keymap.set("n", "<leader>tx", "<cmd>tabclose<CR>", { desc = "Close current tab" }) -- close current tab
 keymap.set("n", "<leader>tn", "<cmd>tabn<CR>", { desc = "Go to next tab" }) --  go to next tab
-keymap.set("n", "<leader>tp", "<cmd>tabp<CR>", { desc = "Go to previous tab" }) --  go to previous tab
-keymap.set("n", "<leader>tf", "<cmd>tabnew %<CR>", { desc = "Open current buffer in new tab" }) --  move current buffer to new tab
+keymap.set("n", "<leader>tb", "<cmd>tabp<CR>", { desc = "Go to previous tab" }) --  go to previous tab
+keymap.set("n", "<leader>tb", "<cmd>tabnew %<CR>", { desc = "Open current buffer in new tab" }) --  move current buffer to new tab
 
 -- visual
 keymap.set("v", "<", "<gv", { desc = "Stay in right indentation mode" }) -- right indentation
 keymap.set("v", ">", ">gv", { desc = "Stay in left indentation mode" }) -- left indentation
+
+-- terraform
+keymap.set("n", "<leader>ti", "<cmd>!terraform init<CR><CR>", { desc = "terraform init" }) -- terraform init
+keymap.set("n", "<leader>tf", "<cmd>!terraform fmt<CR><CR>", { desc = "terraform format" }) -- terraform format 
+keymap.set("n", "<leader>tv", "<cmd>!terraform validate<CR><CR>", { desc = "terraform validate" }) -- terraform validate
+keymap.set("n", "<leader>tp", "<cmd>!terraform plan<CR><CR>", { desc = "terraform plan" }) -- terraform plan
+keymap.set("n", "<leader>ta", "<cmd>!terraform apply<CR><CR>", { desc = "terraform apply" }) -- terraform apply
